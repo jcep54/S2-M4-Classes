cbe34ee795ddb7c3c3396a81f1e1653b8b03e674
Jose Cepero <ceperojose54@gmail.com>
2023-01-28 15:21:25 -0500

    answered all tasks.. still failing a test for task 2

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ class Car {
   }
 
   drive(distance){
+    
     if (distance <= this.milesPerGallon * this.tank){
     this.odometer += distance;
     this.tank -= distance / this.milesPerGallon;
@@ -209,8 +210,18 @@ class Student extends Lambdasian{
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor{
+  constructor(obj){
+  super(obj);
+  this.gradClassName = obj.gradClassName;
+  this.favInstructor = obj.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @${channel} standy times!`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
 
 /*
